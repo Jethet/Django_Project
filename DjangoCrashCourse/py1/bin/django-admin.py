7d5df5d56521dc16800b44692bd5200d8a2ff2a7
Jethet <henriette.hettinga@gmail.com>
2019-05-14 15:55:17 +0200

    Create venv and install packages

---
+++ DjangoCrashCourse/py1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/henriettehettinga/GitHub/Django_Project/DjangoCrashCourse/py1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
