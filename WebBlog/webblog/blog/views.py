b3c098c23422b0180eb5947f3ed9f0c1a135eeef
Jethet <henriette.hettinga@gmail.com>
2019-05-16 16:06:49 +0200

    Test with dummy content

--- WebBlog/webblog/blog/views.py
+++ WebBlog/webblog/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'HH',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 16, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 15, 2019'
+    }
+]
+
 # This function handles the traffic that lands on the homepage of the blog:
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 # This function handles the logic for the 'about'page of the blog:
 def about(request):
