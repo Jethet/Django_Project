cd5094692e70c16af30a5b639b555711262819b2
Jethet <henriette.hettinga@gmail.com>
2019-05-16 12:20:26 +0200

    Instal Django in venv

---
+++ WebBlog/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/henriettehettinga/GitHub/Django_Project/WebBlog/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
