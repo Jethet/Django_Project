717ba71b1b9e3bdf78ab51ceb580a9e6c882cfa5
Jethet <henriette.hettinga@gmail.com>
2019-05-17 15:23:35 +0200

    Create class and attributes for posts

--- WebBlog/webblog/blog/models.py
+++ WebBlog/webblog/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User  # this is a one-to-many relationship
 
-# Create your models here.
+# Django includes various models. These can be used and also adjusted.
+
+# This class inherits from the Django model class. Each class is a table in
+# the database. Each attribute is a different field in the database.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if user is deleted, posts are deleted as well
