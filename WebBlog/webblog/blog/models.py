d2c86e6d11f9b10137b8e074a940e5a1a2fa8aeb
Jethet <henriette.hettinga@gmail.com>
2019-05-17 15:59:34 +0200

    Add dunder str method to print post details

--- WebBlog/webblog/blog/models.py
+++ WebBlog/webblog/blog/models.py
@@ -12,3 +12,6 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE) # if user is deleted, posts are deleted as well
+
+    def __str__(self):
+        return self.title
