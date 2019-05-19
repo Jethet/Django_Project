a867c6ed3a683ea42445df52ce34949cc114a39d
Jethet <henriette.hettinga@gmail.com>
2019-05-19 18:12:12 +0200

    Import Post model and register on admin page

--- WebBlog/webblog/blog/admin.py
+++ WebBlog/webblog/blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+
+# First import the model that you want to show up on the admin page:
 from .models import Post
 
+# Register your models on the admin page:
 admin.site.register(Post)
