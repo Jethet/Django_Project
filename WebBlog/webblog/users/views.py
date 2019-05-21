11094b85009c1e11684610baf36ca1749c754c75
Jethet <henriette.hettinga@gmail.com>
2019-05-21 12:13:53 +0200

    Change line break

--- WebBlog/webblog/users/views.py
+++ WebBlog/webblog/users/views.py
@@ -21,9 +21,7 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                    request.FILES,
-                                    instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
