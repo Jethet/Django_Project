2da2d2c9b02f29c7113544bc22cefd34d6c9f0a6
Jethet <henriette.hettinga@gmail.com>
2019-05-22 16:58:12 +0200

    Add url for new post

--- WebBlog/webblog/blog/urls.py
+++ WebBlog/webblog/blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),  # '' specifies homepage for path
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
 
