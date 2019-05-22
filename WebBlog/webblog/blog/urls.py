53f65d998bef7f54006b8f86f78e6b6c38358b75
Jethet <henriette.hettinga@gmail.com>
2019-05-22 16:32:23 +0200

    Adjust url pattern for class-based views (PostListView, PostDetailView)

--- WebBlog/webblog/blog/urls.py
+++ WebBlog/webblog/blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),   # '' specifies homepage for path
+    path('', PostListView.as_view(), name='blog-home'),  # '' specifies homepage for path
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
+
+
+# With class-based views, the server looks for the following pattern:
+#  <app>/<model>_<viewtype>.html
+# For example:  blog/post_list.html
