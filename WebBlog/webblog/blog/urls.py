e3a48eaf0727f7eb2585042a6a2d30245031f63e
Jethet <henriette.hettinga@gmail.com>
2019-05-23 12:38:32 +0200

    Add PostDeleteView url

--- WebBlog/webblog/blog/urls.py
+++ WebBlog/webblog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
