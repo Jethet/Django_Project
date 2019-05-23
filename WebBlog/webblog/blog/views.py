cf18ddc6eba4195d770e0adafbef699f0349df45
Jethet <henriette.hettinga@gmail.com>
2019-05-23 12:42:13 +0200

    Add url to homepage to redirect after post deletion

--- WebBlog/webblog/blog/views.py
+++ WebBlog/webblog/blog/views.py
@@ -36,6 +36,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
+    success_url = '/'     #If deletion passes, user is directed to homepage
 
     def test_func(self):
         post = self.get_object()
