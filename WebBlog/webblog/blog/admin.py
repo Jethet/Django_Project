f2c85c72d913618952a329da18eb2a11df542a4a
Jethet <henriette.hettinga@gmail.com>
2019-05-30 17:15:16 +0200

    Move WebBlog project to separate repo for deployment with Heroku

--- WebBlog/webblog/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# First import the model that you want to show up on the admin page:
-from .models import Post
-
-# Register your models on the admin page:
-admin.site.register(Post)
