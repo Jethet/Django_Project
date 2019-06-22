8869307fb031c35770b07f640b6c5e4e90abc526
Jethet <henriette.hettinga@gmail.com>
2019-06-22 13:53:41 +0200

    Install Django for project

---
+++ wordcount/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/henriettehettinga/GitHub/DjangoProject-WordCount/wordcount/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
