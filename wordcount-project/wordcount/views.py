762c51d93f8191c0e89e8c3ba762ed07d3d38365
Jethet <henriette.hettinga@gmail.com>
2019-06-22 16:03:10 +0200

    Test homepage template and view

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # function for the home page, to be used in the url path for the home page when
 # the user requests the home page:
 def homepage(request):
-    # what is sent back to the user:
-    return HttpResponse('Hello')
+    # what is sent back to the user: the html for the homepage
+    return render(request, 'home.html')
 
 def firstTest(request):
     return HttpResponse('This is a test')
