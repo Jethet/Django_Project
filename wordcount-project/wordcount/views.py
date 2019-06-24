1f7e98424ca246436676705479a3c9e179351281
Jethet <henriette.hettinga@gmail.com>
2019-06-24 15:33:27 +0200

    Create function to count specific words

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -9,18 +9,20 @@ def homepage(request):
 
 def count(request):
     # this gets the text that is entered by the user:
-    fulltext = request.GET['fulltext']
+    fulltext = request.GET.get('fulltext')
 
     wordlist = fulltext.split()
     # count the number of times a word appears in the text:
-    worddictionary#WHY IS THIS JUST THE VARIABLE AND NO {} ???
+    worddictionary = {}
+
     for word in wordlist:
         if word in worddictionary:
+            # count and increase:
             worddictionary[word] += 1
         else:
+            # if not yet in dictionary, then add word to dictionary:
             worddictionary[word] = 1
-
 
     # this sends the text to the count page to be shown
     # this also includes the counter of the separate words
-    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist), 'worddictionary'=worddictionary.items()})
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist), 'worddictionary':worddictionary.items()})
